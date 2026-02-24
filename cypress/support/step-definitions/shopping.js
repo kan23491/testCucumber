ed89d7da9d357fb2dcf37a12498b178037826d39
v_kanyaratk <OUTSOURCE1765@setth.onmicrosoft.com>
2026-02-24 17:00:16 +0700

    add change wording

--- cypress/support/step-definitions/shopping.js
+++ cypress/support/step-definitions/shopping.js
@@ -1,11 +1,8 @@
-import { expect } from "chai";
 import { Given, When, And, Then } from "cypress-cucumber-preprocessor/steps";
 
 When(/^Click Add to Cart of (Apple iPhone 13|Huawei Mate 20 Lite)$/i, (product) => {
     let position = product === "Apple iPhone 13"? "4" : "2";
-    cy.get(`#prooood .shop-item:nth-child(${position}) button`).click()
-
-    
+    cy.get(`#prooood .shop-item:nth-child(${position}) button`).click()    
 });
 
 And(/^Fill amount as (.+) for (.+)$/i, (amount, product) => {
@@ -18,37 +15,43 @@ And(/^Fill amount as (.+) for (.+)$/i, (amount, product) => {
 })
 
 Then(/^The Total should show result correctly$/i, () => {
+    // price apple
     cy.get(".cart-items > :nth-child(1) > .cart-price")
         .then((ApplePrice) => {
-            const priceApple = ApplePrice.text().split("$")
-             
-            cy.get(':nth-child(1) > .cart-quantity > .cart-quantity-input').invoke("val")
-                .then((appleQuantity) => {
-                    const apple = appleQuantity
-                
+          const priceApple = ApplePrice.text().split("$");
+          // element appleQuantity
+          cy.get(":nth-child(1) > .cart-quantity > .cart-quantity-input")
+            .invoke("val")
+              .then((appleQuantity) => {
+                // element price huawei split $
                 cy.get(".cart-items > :nth-child(2) > .cart-price").then(
-                    (HuaweiPrice) => {
-                     const priceHuawei = HuaweiPrice.text().split("$");
-                        
+                  (HuaweiPrice) => {
+                        const priceHuawei = HuaweiPrice.text().split("$");
+                        // $xxx
+
+                    // element huaweiQuantity
                     cy.get(
-                        ":nth-child(2) > .cart-quantity > .cart-quantity-input"
+                      ":nth-child(2) > .cart-quantity > .cart-quantity-input"
                     )
-                        .invoke("val")
-                        .then((huaweiQuantity) => {  
-                            const totalApple = priceApple[1] * appleQuantity;
-                            const totalHuawei = priceHuawei[1] * huaweiQuantity;
-                            
-                            let resultTotal = totalApple + totalHuawei
-                            
-                            cy.get(".cart-total-price").invoke("text").then((total) => {
-                                expect(total).to.equal(`$${resultTotal}`);
-                                sessionStorage.setItem("total",resultTotal)
-                            })
-                        });
-                    }
+                      .invoke("val")
+                        .then((huaweiQuantity) => {
+                          
+                        const totalApple = priceApple[1] * appleQuantity;
+                        const totalHuawei = priceHuawei[1] * huaweiQuantity;
+
+                        let resultTotal = totalApple + totalHuawei;
+
+                        cy.get(".cart-total-price")
+                          .invoke("text")
+                          .then((total) => {
+                            expect(total).to.equal(`$${resultTotal}`);
+                            sessionStorage.setItem("total", resultTotal);
+                          });
+                      });
+                  }
                 );
-            });  
-      }); 
+              });
+        }); 
 })
 
 And(/^Select Country as "(.+)" on Shipping Details$/i, (value) => {
@@ -64,14 +67,9 @@ Then(/^System should show message submit success$/i, () => {
         const city = win.sessionStorage.getItem("city");
         const country = win.sessionStorage.getItem("country");
         const total = win.sessionStorage.getItem("total");
-        console.log(street, city,country);
-        
-
         cy.get("#message").invoke("text").then((message) => {
-            console.log(message);
-            
         expect(message).to.equal(
-          `Congrats! Your order of  $2546.34  has been registered and will be shipped to 5878 little streets, Change mai - Thailand.`
+          `Congrats! Your order of $${total}  has been registered and will be shipped to ${street}, ${city} - ${country}.`
         );
     })
 
